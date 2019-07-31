546022697380e7a311e4b2cdda02b4a7973ceb09
t-murata <t-murata@townsystem.co.jp>
2019-07-31 20:47:47 +0900

    PATCHリクエストの場合、更新日時を更新しないように変更

--- middleware.js
+++ middleware.js
@@ -1,5 +1,5 @@
 module.exports = function (req, res, next) {
-  if (['POST', 'PUT', 'PATCH'].includes(req.method)) {
+  if (['POST', 'PUT'].includes(req.method)) {
     // Converts POST to GET and move payload to query params
     // This way it will make JSON Server that it's GET request
     req.query = req.body
