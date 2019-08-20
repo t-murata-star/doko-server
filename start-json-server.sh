8aab743cae724cdab956709231c8a9cbf8a4d328
t-murata <t-murata@townsystem.co.jp>
2019-08-20 11:05:31 +0900

    json-serverをデーモン化するための起動バッチファイルを追加

---
+++ start-json-server.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+PATH=$PATH:/usr/sbin:/home/ec2-user/.nvm/versions/node/v12.7.0/bin:/home/ec2-user/.local/bin:/home/ec2-user/bin
+
+cd /home/ec2-user/doko-servser-mock
+/home/ec2-user/.nvm/versions/node/v12.7.0/bin/npm run start:auth
+
+
