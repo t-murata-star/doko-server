48684ee420d70c0309a73c6ad2da5d065edde505
t-murata <t-murata@townsystem.co.jp>
2020-02-21 18:37:08 +0900

    リビジョン 6001017 のコミット漏れ

--- start-json-server.sh
+++ start-json-server.sh
@@ -2,7 +2,5 @@
 
 PATH=$PATH:/usr/sbin:/home/ec2-user/.nvm/versions/node/v12.7.0/bin:/home/ec2-user/.local/bin:/home/ec2-user/bin
 
-cd /home/ec2-user/doko-servser-mock
+cd /home/ec2-user/doko-servser
 /home/ec2-user/.nvm/versions/node/v12.7.0/bin/npm run start:auth
-
-
